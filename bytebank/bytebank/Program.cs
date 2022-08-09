3e58f1eb19f6cfb2e52f6885f940395bbf3db121
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-09 19:23:14 -0300

    Instancing CheckingAccount class.

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -1,15 +1,20 @@
-﻿Console.WriteLine("Welcome to ByteBank!");
+﻿using bytebank;
 
-string holder = "Gabriel de Almeida";
-string account = "10123-X";
-int agencyNumber = 23;
-string agencyName = "Central Agency";
-double balance = 93.50;
+Console.WriteLine("Welcome to ByteBank!");
 
-Console.WriteLine($"Holder: {holder}");
-Console.WriteLine($"Account: {account}");
-Console.WriteLine($"Agency Number: {agencyNumber}");
-Console.WriteLine($"Agency Name: {agencyName}");
-Console.WriteLine($"Balance: {balance}");
+CheckingAccount firstAccount = new CheckingAccount();
+firstAccount.holder = "Gabriel de Almeida";
+firstAccount.account = "10123-X";
+firstAccount.agencyNumber = 23;
+firstAccount.agencyName = "Central Agency";
+firstAccount.balance = 102.30;
+
+CheckingAccount secondAccount = new CheckingAccount();
+
+Console.WriteLine($"Holder: {firstAccount.holder}");
+Console.WriteLine($"Account: {firstAccount.account}");
+Console.WriteLine($"Agency Number: {firstAccount.agencyNumber}");
+Console.WriteLine($"Agency Name: {firstAccount.agencyName}");
+Console.WriteLine($"Balance: {firstAccount.balance}");
 
 Console.ReadKey();
