01376f121454abf9b125ec2a002a97ca4d7c9111
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-09 18:55:46 -0300

    Adding and printing bank account info.

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to ByteBank!");
+
+string holder = "Gabriel de Almeida";
+string account = "10123-X";
+int agencyNumber = 23;
+string agencyName = "Central Agency";
+double balance = 93.50;
+
+Console.WriteLine($"Holder: {holder}");
+Console.WriteLine($"Account: {account}");
+Console.WriteLine($"Agency Number: {agencyNumber}");
+Console.WriteLine($"Agency Name: {agencyName}");
+Console.WriteLine($"Balance: {balance}");
+
+Console.ReadKey();
