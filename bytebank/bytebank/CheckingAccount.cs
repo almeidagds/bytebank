63c8ce6baa400493cccdb5187c1472f043d2c30f
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-09 19:10:27 -0300

    Creating class CheckingAccount.

---
+++ bytebank/bytebank/CheckingAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace bytebank {
+    public class CheckingAccount {
+        public string holder;
+        public string account;
+        public int agencyNumber;
+        public string agencyName;
+        public double balance;
+    }
+}
