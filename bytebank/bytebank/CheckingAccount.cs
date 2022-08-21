093fb014f761ce12c59c1e2af5060263b84fcd02
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-21 20:33:21 -0300

    Creating 'Client' class.

--- bytebank/bytebank/CheckingAccount.cs
+++ bytebank/bytebank/CheckingAccount.cs
@@ -1,6 +1,6 @@
 ﻿namespace bytebank {
     public class CheckingAccount {
-        public string holder;
+
         public string account;
         public int agencyNumber;
         public string agencyName;
--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -2,19 +2,10 @@
 
 Console.WriteLine("Welcome to ByteBank!");
 
-CheckingAccount firstAccount = new CheckingAccount();
-firstAccount.holder = "Gabriel de Almeida";
-firstAccount.account = "10123-X";
-firstAccount.agencyNumber = 23;
-firstAccount.agencyName = "Central Agency";
-firstAccount.balance = 102.30;
+Client client = new Client();
 
-CheckingAccount secondAccount = new CheckingAccount();
-
-Console.WriteLine($"Holder: {firstAccount.holder}");
-Console.WriteLine($"Account: {firstAccount.account}");
-Console.WriteLine($"Agency Number: {firstAccount.agencyNumber}");
-Console.WriteLine($"Agency Name: {firstAccount.agencyName}");
-Console.WriteLine($"Balance: {firstAccount.balance}");
+client.name = "Gabriel";
+client.cpf = "12345678912";
+client.job = "Desenvolvedor Júnior";
 
 Console.ReadKey();
