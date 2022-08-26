d51cb088aaab09ded7aaebb0bb36ac655395735a
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-26 20:43:24 -0300

    Using 'this' to define Employee type on constructor method.

--- bytebank/bytebank/Employees/Employee.cs
+++ bytebank/bytebank/Employees/Employee.cs
@@ -8,12 +8,22 @@ namespace bytebank.Employees
 {
     public class Employee
     {   
+        public Employee(int type)
+        {
+            this._type = type;
+        }
+
+        private int _type;
         public string Name { get; set; }
         public string Cpf { get; set; }
         public double Salary { get; set; }
 
         public double getBonus()
         {
+            if (_type === 1)
+            {
+                return Salary;
+            }
             return Salary * 0.1;
         }
     }
