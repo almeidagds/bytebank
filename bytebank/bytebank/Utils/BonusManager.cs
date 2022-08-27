32c468e61f192d6306416c54274b7ffb6aaf5e34
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-26 21:02:09 -0300

    Creating 'Utils' and a 'BonusManager' class.

---
+++ bytebank/bytebank/Utils/BonusManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytebank.Employees;
+
+namespace bytebank.Utils
+{
+    internal class BonusManager
+    {
+        private double _bonusTotal;
+
+        public void Registry(Employee employee)
+        {
+            this._bonusTotal += employee.getBonus();
+        }
+        public double GetBonusTotal()
+        {
+            return this._bonusTotal;
+        }
+    }
+}
