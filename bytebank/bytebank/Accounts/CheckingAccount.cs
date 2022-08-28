e0d991ef2f0530dcc6ed75bdf5179f93c2d4cba9
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-28 13:29:05 -0300

    Creating Client and CheckingAccount classes.

---
+++ bytebank/bytebank/Accounts/CheckingAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytebank.Clients;
+
+namespace bytebank.Accounts
+{
+    public class CheckingAccount
+    {
+        public Client Holder { get; set; }
+        public int Agency
+        {
+            get { return Agency; }
+            set
+            {
+                if (value > 0) Agency = value;
+            }
+        }
+        public int AccountNumber
+        {
+            get { return AccountNumber; }
+            set
+            {
+                if (value > 0) AccountNumber = value;
+            }
+        }
+        public double Balance
+        {
+            get { return Balance; }
+            set
+            {
+                if (value > 0) Balance = value;
+            }
+        }
+        public static int NumberOfAccounts { get; private set; }
+
+        public bool Withdraw(double value)
+        {
+            bool withdrawWasSuccessfull = false;
+
+            if (Balance >= value)
+            {
+                Balance -= value;
+                withdrawWasSuccessfull = true;
+            }
+
+            return withdrawWasSuccessfull;
+        }
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public bool MakeWireTransfer(double value, CheckingAccount destinyAccount)
+        {
+            bool wireTransferWasSuccesfful = false;
+
+            if (Balance >= value)
+            {
+                Balance -= value;
+                destinyAccount.Deposit(value);
+                wireTransferWasSuccesfful = true;
+            }
+
+            return wireTransferWasSuccesfful;
+        }
+    }
+}
---
+++ bytebank/bytebank/Clients/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank.Clients
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+
+        public Client(string name, string cpf, string profissao)
+        {
+            Name = name;
+            Cpf = cpf;
+            Profissao = profissao;
+        }
+    }
+}
