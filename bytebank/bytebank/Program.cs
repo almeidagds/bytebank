4dd7343cfe4df8c4fa030da4eb5b13ebb7cd7cb3
Gabriel de Almeida <82405235+almeidagds@users.noreply.github.com>
2022-10-01 18:54:08 -0300

    Cleaning main file.

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -9,14 +9,3 @@ using System.Runtime.Serialization.Formatters;
 using System.Collections;
 using System.Security.AccessControl;
 using bytebank.Attendance;
-
-DateTime paymentFinalDate = new DateTime(2022,12,05);
-DateTime currentDate = DateTime.Now;
-TimeSpan diff = paymentFinalDate - currentDate;
-
-Console.WriteLine($"Vencimento em: {GetLegibleTimeInterval(diff)}.");
-
-static string GetLegibleTimeInterval(TimeSpan time)
-{
-    return time.Days + " dias";
-}
