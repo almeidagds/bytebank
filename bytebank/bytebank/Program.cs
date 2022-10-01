01458093f511a2ad515c896980dc3e2f838b76e0
Gabriel de Almeida <almeidag2001@gmail.com>
2022-09-30 22:38:00 -0300

    Working with dates.

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -9,3 +9,14 @@ using System.Runtime.Serialization.Formatters;
 using System.Collections;
 using System.Security.AccessControl;
 using bytebank.Attendance;
+
+DateTime paymentFinalDate = new DateTime(2022,12,05);
+DateTime currentDate = DateTime.Now;
+TimeSpan diff = paymentFinalDate - currentDate;
+
+Console.WriteLine($"Vencimento em: {GetLegibleTimeInterval(diff)}.");
+
+static string GetLegibleTimeInterval(TimeSpan time)
+{
+    return time.Days + " dias";
+}
