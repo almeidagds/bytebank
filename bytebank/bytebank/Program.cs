3d1b55635d981a61cc3ceb783fc108ce06f56000
Gabriel de Almeida <almeidag2001@gmail.com>
2022-10-02 22:03:25 -0300

    Finishing documentation and nuget course.

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -9,3 +9,14 @@ using System.Runtime.Serialization.Formatters;
 using System.Collections;
 using System.Security.AccessControl;
 using bytebank.Attendance;
+using Humanizer;
+
+DateTime paymentFinalDate = new DateTime(2022,10,10);
+DateTime currentDateTime = DateTime.Now;
+
+TimeSpan timeDiff = paymentFinalDate - currentDateTime;
+
+string message = $"Due on {TimeSpanHumanizeExtensions.Humanize(timeDiff)}.";
+
+Console.WriteLine(message);
+Console.ReadLine();
